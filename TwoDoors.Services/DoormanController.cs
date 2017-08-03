903f6c527505c83169f51c24bf57e5bcb7fdee38
tireis <tireis@outlook.com>
2017-08-04 00:21:28 +0200

    Adds swagger.

---
+++ TwoDoors.Services/DoormanController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace TwoDoors.Services
+{
+    [RoutePrefix("api/doorman")]
+    public class DoormanController : ApiController
+    {
+        [HttpGet]
+        [Route("test")]
+        public IHttpActionResult Get()
+        {
+            return Ok(42);
+        }
+    }
+}
